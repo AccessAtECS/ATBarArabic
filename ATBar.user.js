0b7bd976db49b9ce85954960538bd9abf513d18f
Sebastian Skuse <seb@skuse.net>
2012-03-21 16:30:59 +0000

    Use the word prediction plugin
    Signed-off-by: Sebastian Skuse <seb@skuse.net>

--- ATBar.user.js
+++ ATBar.user.js
@@ -31,12 +31,12 @@ function __start(){
 		$lib = AtKit.lib();
 		
 		var settings = {
-			'version': '2.0.141-beta9'
+			'version': '2.0.144-beta9'
 		};
 		
 		settings.baseURL = ('https:' == document.location.protocol ? 'https://ssl.atbar.org/c/ATBar2/' : 'http://c.atbar.org/ATBar2/');
 		
-		var plugins = ["resize", "fonts", "spellng", "dictionary", "insipio-tts", "css", "shortcutkeys", "tooltip"];
+		var plugins = ["resize", "fonts", "spellng", "dictionary", "insipio-tts", "wordprediction", "css", "shortcutkeys", "tooltip"];
 		
 		var onLoad = function(){
 		
